1ba8e8aeb541059a2ed5c493b866d298a25fbfe5
Carl <carl@edgeimpulse.com>
2024-04-17 21:42:31 +0100

    back to hard coded dims as dynamic isn't working properly

--- nvidia_tao_tf1/cv/yolo_v3/architecture/yolo_arch.py
+++ nvidia_tao_tf1/cv/yolo_v3/architecture/yolo_arch.py
@@ -234,14 +234,14 @@ def YOLO(input_tensor,  # pylint: disable=W0102
         yolo_fcn = [qat_model.get_layer('conv_big_object').output,
                     qat_model.get_layer('conv_mid_object').output,
                     qat_model.get_layer('conv_sm_object').output]
-    bgdet_dim = yolo_fcn[0].shape[1].value * yolo_fcn[0].shape[2].value * yolo_fcn[0].shape[3].value / (num_classes + 5) 
-    bgdet = Reshape((int(bgdet_dim), num_classes + 5),
+    #bgdet_dim = yolo_fcn[0].shape[1].value * yolo_fcn[0].shape[2].value * yolo_fcn[0].shape[3].value / (num_classes + 5) 
+    gdet = Reshape((int(147), num_classes + 5),
                     name="bg_reshape")(Permute((2, 3, 1), name="bg_permute")(yolo_fcn[0]))
-    mgdet_dim = yolo_fcn[1].shape[1].value * yolo_fcn[1].shape[2].value * yolo_fcn[1].shape[3].value / (num_classes + 5) 
-    mddet = Reshape((int(mgdet_dim), num_classes + 5),
+    #mgdet_dim = yolo_fcn[1].shape[1].value * yolo_fcn[1].shape[2].value * yolo_fcn[1].shape[3].value / (num_classes + 5) 
+    mddet = Reshape((int(588), num_classes + 5),
                     name="md_reshape")(Permute((2, 3, 1), name="md_permute")(yolo_fcn[1]))
-    smdet_dim = yolo_fcn[2].shape[1].value * yolo_fcn[2].shape[2].value * yolo_fcn[2].shape[3].value / (num_classes + 5) 
-    smdet = Reshape((int(smdet_dim), num_classes + 5),
+    #smdet_dim = yolo_fcn[2].shape[1].value * yolo_fcn[2].shape[2].value * yolo_fcn[2].shape[3].value / (num_classes + 5) 
+    smdet = Reshape((int(2352), num_classes + 5),
                     name="sm_reshape")(Permute((2, 3, 1), name="sm_permute")(yolo_fcn[2]))
 
     # build YOLO v3 anchor layers for corresponding feature maps. Anchor shapes are defined in args.
